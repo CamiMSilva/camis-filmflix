00ea8dfae9d59506638d968f2d893f92f7ad7778
Camila M. Silva <cms.mila@yahoo.com.br>
2022-01-18 14:47:14 -0300

    Geração dos módulos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'camis-filmflix',
+    appId: '1:459050495267:web:04895772a7dced01497096',
+    storageBucket: 'camis-filmflix.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCUDpkZpYsaDKrW4_a4NV9reNSjFmg8DGk',
+    authDomain: 'camis-filmflix.firebaseapp.com',
+    messagingSenderId: '459050495267',
+  },
   production: true
 };
