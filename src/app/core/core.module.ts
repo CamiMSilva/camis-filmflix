fa718aa7a6248419fc9e348996384a49c313316e
Camila M. Silva <cms.mila@yahoo.com.br>
2022-01-21 16:52:58 -0300

    Add guarda, pág 404 e setups de serviços tmdb

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../shared/material/material/material.module';
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
